982582d5b72295db31b1a86c520b0afe8d7ed95b
meghanas2806 <meghanas2806@gmail.com>
2022-05-24 17:59:39 +0200

    Python style convention - PEP8

--- Store_API/Source/Test.py
+++ Store_API/Source/Test.py
@@ -13,60 +13,58 @@ class TestStore:
     def __init__(
             self, status):
         self.status = status
-         
-        
+
     def get_data(
-            self,product):
-        assert self.status == 0,"Wrong flag set to status"
+            self, product):
+        assert self.status == 0, "Wrong flag set to status"
         self.product = product
-        if(self.status==0):
+        if(self.status == 0):
             fileObject = open("products_test.json", "r")
             jsonContent = fileObject.read()
             aList = json.loads(jsonContent)
-            assert self.product in aList,"Product not available in store"
+            assert self.product in aList, "Product not available in store"
             self.initial_amount = int(aList[self.product]["amount"])
-            assert self.initial_amount > 0,"Quantity should be greater than zero"
+            assert self.initial_amount > 0, "Quantity should be greater than zero"
             fileObject.close()
-            
+
     def put_data(
             self, buy_amount):
-        assert self.status == 0,"Wrong flag set to status"
+        assert self.status == 0, "Wrong flag set to status"
         self.buy_amount = buy_amount
-        with open("products_test.json",'r+') as fileObject:
-                # First we load existing data into a dict.
-                aList = json.load(fileObject)
-                assert self.buy_amount <= self.initial_amount,"Buy amount is greater than initial amount"
-                self.final_state = self.initial_amount - self.buy_amount
-                aList[self.product].update({"amount":str(self.final_state)})
-                # Sets file's current position at offset.
-                fileObject.seek(0)
-                # convert back to json.
-                json.dump(aList, fileObject,indent =4,sort_keys=True)
-    
+        with open("products_test.json", 'r+') as fileObject:
+            # First we load existing data into a dict.
+            aList = json.load(fileObject)
+            assert self.buy_amount <= self.initial_amount, "Buy amount is greater than initial amount"
+            self.final_state = self.initial_amount - self.buy_amount
+            aList[self.product].update({"amount": str(self.final_state)})
+            # Sets file's current position at offset.
+            fileObject.seek(0)
+            # convert back to json.
+            json.dump(aList, fileObject, indent=4, sort_keys=True)
+
     def get_products(
             self):
-        assert self.status == 1,"Wrong flag set to status"
-        if(self.status==1):
+        assert self.status == 1, "Wrong flag set to status"
+        if(self.status == 1):
             fileObject = open("products.json", "r")
             jsonContent = fileObject.read()
             aList = json.loads(jsonContent)
             for i in aList["Products"]:
-                if (int(aList[i]["amount"])>0):
+                if (int(aList[i]["amount"]) > 0):
                     print('/'+i+'/'+str(aList[i]["amount"]))
             fileObject.close()
-    
-    
-    
+
+
 if __name__ == '__main__':
-    
-    store_obj = TestStore(0)#To get product status
-    
+
+    store_obj = TestStore(0)  # To get product status
+
     store_obj.get_data("Apple")
-    
+
     store_obj.put_data(35)
-    
-    store_obj = TestStore(1)#To get shop status
-    
+
+    store_obj = TestStore(1)  # To get shop status
+
     store_obj.get_products()
-    
+
     print("Everything is correct!")
