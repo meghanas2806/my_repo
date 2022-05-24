fdfc224ca9836376e6bec10d65ac7db785fc1ddf
meghanas2806 <meghanas2806@gmail.com>
2022-05-24 17:56:02 +0200

    Python style convention - PEP8

--- Store_API/Source/Store.py
+++ Store_API/Source/Store.py
@@ -12,51 +12,49 @@ class Store:
     def __init__(
             self, status):
         self.status = status
-       
-    
+
     def get_data(
-            self,product):
+            self, product):
         self.product = product
-        if(self.status==0):
+        if(self.status == 0):
             fileObject = open("products.json", "r")
             jsonContent = fileObject.read()
             aList = json.loads(jsonContent)
             self.initial_amount = int(aList[self.product]["amount"])
             print('/'+self.product+'/'+str(aList[self.product]["amount"]))
             fileObject.close()
-       
-    
+
     def put_data(
             self, buy_amount):
         self.buy_amount = buy_amount
-        with open("products.json",'r+') as fileObject:
-                # First we load existing data into a dict.
-                aList = json.load(fileObject)
-                self.final_state = self.initial_amount - self.buy_amount
-                if (self.final_state>9):
-                    aList[self.product].update({"amount":str(self.final_state)})
-                else:
-                    aList[self.product].update({"amount":str(0)+str(self.final_state)})
-                # Sets file's current position at offset.
-                fileObject.seek(0)
-                # convert back to json.
-                json.dump(aList, fileObject,indent =4,sort_keys=True)
-                
+        with open("products.json", 'r+') as fileObject:
+            # First we load existing data into a dict.
+            aList = json.load(fileObject)
+            self.final_state = self.initial_amount - self.buy_amount
+            if (self.final_state > 9):
+                aList[self.product].update({"amount": str(self.final_state)})
+            else:
+                aList[self.product].update(
+                    {"amount": str(0)+str(self.final_state)})
+            # Sets file's current position at offset.
+            fileObject.seek(0)
+            # convert back to json.
+            json.dump(aList, fileObject, indent=4, sort_keys=True)
+
     def get_products(
             self):
-        if(self.status==1):
+        if(self.status == 1):
             fileObject = open("products.json", "r")
             jsonContent = fileObject.read()
             aList = json.loads(jsonContent)
             for i in aList["Products"]:
-                if (int(aList[i]["amount"])>0):
+                if (int(aList[i]["amount"]) > 0):
                     print('/'+i+'/'+str(aList[i]["amount"]))
             fileObject.close()
-    
+
     def get_purchase_data(
             self):
-        if(self.status==0):
-            if (self.final_state >= 0):                    
+        if(self.status == 0):
+            if (self.final_state >= 0):
                 print('/'+self.product+'/'+str(self.initial_amount)+'/'
-                      +str(self.buy_amount)+'/'+str(self.final_state))
-            
+                      + str(self.buy_amount)+'/'+str(self.final_state))
--- Store_API/Source/Transaction.py
+++ Store_API/Source/Transaction.py
@@ -20,25 +20,26 @@ if __name__ == "__main__":
     try:
         arg, val = getopt.getopt(argList, options, long_options)
         for curr_arg, curr_val in arg:
-            if  curr_arg in ("-s", "--shopstatus"):
+            if curr_arg in ("-s", "--shopstatus"):
                 print("Status of the Shop\n")
-                store_obj = Store(1) #To get status of the shop
+                store_obj = Store(1)  # To get status of the shop
                 store_obj.get_products()
             elif curr_arg in ("-b", "--buy"):
                 if (len(sys.argv[2]) > 0 and len(sys.argv[3]) > 0
                         and int(sys.argv[3]) > 0):
-                    store_obj = Store(0) #To get status of the product
+                    store_obj = Store(0)  # To get status of the product
                     var1 = sys.argv[2]  # Product name
                     print("\nCurrent Product Status\n")
                     store_obj.get_data(var1)
-                    #Display data
+                    # Display data
                     print("\nPurchase data\n")
                     var2 = int(sys.argv[3])  # buy amount
                     if (var2 <= store_obj.initial_amount):
                         store_obj.put_data(var2)
                         store_obj.get_purchase_data()
                     else:
-                        print(f"Enter amount lesser than or equal to {store_obj.initial_amount}")
+                        print(
+                            f"Enter amount lesser than or equal to {store_obj.initial_amount}")
                 else:
                     sys.argv[2] = 'Fruit'
                     sys.argv[3] = 0
